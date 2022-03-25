8b6464806ebf4cd6b9863379b0490559df08e773
adarsh <adarshurmaliya@gmail.com>
2022-03-25 14:45:15 +0530

    initial-commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Styles/Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+       <img className='header_logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" />
+       
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: "AIzaSyAFGIx6PjSEzqj9nRcd3FekVyBdVhsVWw8",
+   authDomain: "clone-f6b6a.firebaseapp.com",
+   projectId: "clone-f6b6a",
+   storageBucket: "clone-f6b6a.appspot.com",
+   messagingSenderId: "618671342551",
+   appId: "1:618671342551:web:2bfa8144089852b6eb8192",
+   measurementId: "G-15EF4LZGDX"
+ };
